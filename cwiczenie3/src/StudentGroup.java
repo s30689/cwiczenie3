d3a73ca3a6bf1ef7cea6bbd236ad513fdd2ca7c5
Twojastara2137 <superninja552@gmail.com>
2024-04-04 18:02:39 +0200

    27.03

--- cwiczenie3/src/StudentGroup.java
+++ cwiczenie3/src/StudentGroup.java
@@ -1,6 +1,34 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 public class StudentGroup {
-    public String nazwa;
+    public String name;
+    public List<Student> students = new ArrayList<>();
+
+    public StudentGroup(String name) {
+
+        this.name = name;
+
+    }
+
+    public void addStudent(Student student) {
+
+        if (students.size() >= 15) {
+            throw new IllegalArgumentException("Grupa jest full");
+        } else if (students.contains(student)) {
+            throw new IllegalArgumentException("Stundent "+ student.fname+" "+student.lname+" jest juz dodany");
+        }
+        else {
+
+            this.students.add(student);
+
+        }
+    }
 
 
-
+    @Override
+    public String toString() {
+        return "StudentGroup{" + "name='" + name + ' ' + ", students=" + students + '}';
+    }
 }
